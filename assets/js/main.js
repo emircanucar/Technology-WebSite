a202c6817e1da7364929ff675e5444586423c982
emircanucar <133085075+emircanucar@users.noreply.github.com>
2024-05-12 11:53:06 +0300

    Footer bg section added and responsive improved

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,33 @@ var hamburgerMenu = document.querySelector(".navbar-hamburger");
 function addChange() {
   hamburgerMenu.classList.toggle("change");
 }
+hamburgerMenu.addEventListener("click", addChange);
+
+var categoryMenu = document.querySelector(".category-menu");
+function addShowClass() {
+  categoryMenu.classList.toggle("show");
+}
+
+hamburgerMenu.addEventListener("click", addShowClass);
+
+var swiper = new Swiper(".heroSlider", {});
+var swiper = new Swiper(".postSlider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoPlay: true,
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
